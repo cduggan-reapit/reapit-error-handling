c5d26ecf05a889f90c36824ee88f0452c45df224
cduggan <cduggan@reapit.com>
2024-02-22 12:53:33 +0000

    test: add service collection tests

---
+++ src/Reapit.Packages.ErrorHandling.UnitTests/StartupTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.Extensions.DependencyInjection;
+using Reapit.Packages.ErrorHandling.Middleware;
+
+namespace Reapit.Packages.ErrorHandling.UnitTests;
+
+public class StartupTests
+{
+    // RegisterErrorHandlerServices
+    
+    [Fact]
+    public void RegisterErrorHandlerServices_AddsServices_ToGivenContainer()
+    {
+        var container = GetServiceCollection();
+        container.RegisterErrorHandlerServices();
+        
+        using var provider = container.BuildServiceProvider();
+        
+        // Check that the exception handler is registered in the service collection
+        var exceptionHandler = provider.GetRequiredService<IExceptionHandler>();
+        exceptionHandler.Should().NotBeNull();
+        exceptionHandler.GetType().Should().Be(typeof(UnhandledExceptionHandler));
+    }
+    
+    // Private methods
+
+    private static IServiceCollection GetServiceCollection()
+    {
+        var services = new ServiceCollection();
+        services.AddLogging();
+        return services;
+    }
+}
--- src/Reapit.Packages.ErrorHandling/Startup.cs
+++ src/Reapit.Packages.ErrorHandling/Startup.cs
@@ -3,8 +3,15 @@ using Reapit.Packages.ErrorHandling.Middleware;
 
 namespace Reapit.Packages.ErrorHandling;
 
+/// <summary>
+/// Service registration methods for the <see cref="ErrorHandling"/> project
+/// </summary>
 public static class Startup
 {
+    /// <summary>
+    /// Register error handling services in the application service collection
+    /// </summary>
+    /// <param name="services">The application service collection</param>
     public static IServiceCollection RegisterErrorHandlerServices(this IServiceCollection services)
     {
         services.AddExceptionHandler<UnhandledExceptionHandler>();
